9289316b95f000b66c8d23cb516e2a60a5ec185c
Malick <diop-malick@hotmail.fr>
2016-03-22 23:47:17 +0100

    modify item of list primitives

--- src/Collection/List_ModifyInLoop.java
+++ src/Collection/List_ModifyInLoop.java
@@ -1,6 +1,8 @@
 package Collection;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import utils.Person;
 import utils.PersonsDB;
@@ -10,12 +12,28 @@ public class List_ModifyInLoop {
 	public static void main(String[] args) {
 		
 		List<Person> personnes = PersonsDB.PERSONS;
-		System.out.println("===================== Start Person List ===============");
+		System.out.println("===================== Started Person List ===============");
 		display(personnes);
-				
+			
+		// loop and modify
 		personnes.stream().filter(item -> (item.getAge() == 63)).forEach(p -> p.setAge(0));
-		System.out.println("===================== update Person List ===============");
+		
+		System.out.println("===================== updated Person List ===============");
 		display(personnes);
+		
+		System.out.println("===================== started primitive List ===============");
+		List<String> list = new ArrayList<>();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+	    System.out.println(list);
+	    
+		System.out.println("===================== upated primitive List ===============");
+		list = list.stream().map(x -> "x-" + x).collect(Collectors.toList());
+		System.out.println("replace some item " + list);
+		
+		list.replaceAll(x -> "b" + x);
+		System.out.println("replace All item " + list);
 	}
 	
 	public static void display(List<Person> personnes) {
