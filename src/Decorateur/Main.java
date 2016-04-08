93fb98b2ca01a03c5f06deb94c10d9c4794127b3
Malick <diop-malick@hotmail.fr>
2016-04-08 11:47:04 +0200

    delete no need class

--- src/Decorateur/Main.java
+++ src/Decorateur/Main.java
@@ -1,5 +1,11 @@
 package Decorateur;
 
+import Decorateur.example1.Boisson;
+import Decorateur.example1.Deca;
+import Decorateur.example1.Espresso;
+import Decorateur.example1.dec.Chocolat;
+import Decorateur.example1.dec.Lait;
+
 public class Main {
 
 	/**
@@ -19,6 +25,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// on crée un boisson
+		Boisson expresso = new Espresso();
+		System.out.println(expresso.getDescription());
+		System.out.println(expresso.cout());
+
+		// on décore l'objet boisson avec du lait
+		expresso = new Lait(expresso);
+		System.out.println(expresso.getDescription());
+		System.out.println(expresso.cout());
+
+		// on décore l'objet boisson avec du lait
+		expresso = new Chocolat(expresso);
+		System.out.println(expresso.getDescription());
+		System.out.println(expresso.cout());
+
+		// on crée un boisson
+		Boisson deca = new Chocolat(new Lait(new Deca()));
+		System.out.println(deca.getDescription());
+		System.out.println(deca.cout());
 
 	}
 
