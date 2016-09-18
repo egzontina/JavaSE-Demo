1aeea9259d75fed2799966b2b70ede5f02c5bd5d
Malick <diop-malick@hotmail.fr>
2016-09-18 19:50:08 +0200

    Codin game : update scii art puzzle

--- codingame/src/main/java/puzzles/facile/AsciiArt.java
+++ codingame/src/main/java/puzzles/facile/AsciiArt.java
@@ -8,45 +8,110 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
- * L'art ASCII permet de représenter des formes en utilisant des caractères. 
+ * L'art ASCII permet de représenter des formes en utilisant des caractères.
+ * 
  * <pre>
  * ASCII : American Standard Code for Information Interchange
  * contient les caractères nécessaires pour écrire en anglais
  * définit 128 caractères numérotés de 0 à 127 et codés en binaire de 0000000 à 1111111.
- *  7 bits suffisent pour représenter un caractère codé en ASCII
+ * 7 bits suffisent pour représenter un caractère codé en ASCII
  * </pre>
- * @see https://www.codingame.com/ide/5254062c7332a8af3f50058007ff7e609b39fb4
  * 
+ * <pre>
+ * In stations and airports you often see this type of screen. Have you ever asked yourself how it might be possible to simulate this display on a good old terminal? We have: with ASCII art!
+ * 
+ * ASCII art allows you to represent forms by using characters. To be precise, in our case, these forms are words. For example, the word "MANHATTAN" could be displayed as follows in ASCII art:
+ * # #  #  ### # #  #  ### ###  #  ###
+ * ### # # # # # # # #  #   #  # # # #
+ * ### ### # # ### ###  #   #  ### # #
+ * # # # # # # # # # #  #   #  # # # #
+ * # # # # # # # # # #  #   #  # # # #
+ * ​Your mission is to write a program that can display a line of text in ASCII art.
+ * 
+ * INPUT:
+ * Line 1: the width L of a letter represented in ASCII art. All letters are the same width.
+ * Line 2: the height H of a letter represented in ASCII art. All letters are the same height.
+ * Line 3: The line of text T, composed of N ASCII characters.
+ * Following Lines: the string of characters ABCDEFGHIJKLMNOPQRSTUVWXYZ? Represented in ASCII art.
+ * 
+ * OUTPUT:
+ * The text T in ASCII art.
+ * The characters a to z are shown in ASCII art by their equivalent in upper case.
+ * The characters which are not in the intervals [a-z] or [A-Z] will be shown as a question mark in ASCII art.
+ * 
+ * CONSTRAINTS :
+ * 0 < L < 30
+ * 0 < H < 30
+ * 0 < N < 200
+ * </pre>
+ * 
+ * @see https://www.codingame.com/ide/5254062c7332a8af3f50058007ff7e609b39fb4
  * @author Malick
- *
  */
 class AsciiArt {
 
+    // ASCII-art representation of the whole alphabet + ?
+    static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+
+    static final char FIRST_CHAR = 'A';
+    static final char LAST_CHAR = 'Z';
+
     /**
-     * Main MEHTOD
-     * <p>
-     * programme capable d'afficher une ligne de texte en art ASCII.
-     * @param args
+     * Main Method
      */
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
+
+        // width and height of the ASCII-art representation of 1 character
         int L = in.nextInt();
         int H = in.nextInt();
+
+        // text to convert
         in.nextLine();
         String T = in.nextLine();
+
+        // To prevent minuscule char like 'a'
+        String text = T.toUpperCase();
+
+        // ASCII art representations of the whole alphabet + ?
+        ArrayList<String> ascii = new ArrayList<String>();
+
         for (int i = 0; i < H; i++) {
-            String ROW = in.nextLine();
+            ascii.add(in.nextLine());
         }
 
-        
-     // Regular expression pattern to test input        
-        if (T.matches("[a-zA-Z]+")) {
-            
-        }
-        
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
+        // Debug Messages
+        // System.err.println("L : Width ... :" + L);
+        // System.err.println("H : Higth ... :" + H);
+        // System.err.println("T : Text Line... :" + T);
+        // System.err.println("List ASCII");
+        // ascii.forEach(System.err::println);
 
-        System.out.println("answer");
+        // find the necessary indexes of letters in alphabet string
+        ArrayList<Integer> indexes = new ArrayList<Integer>();
+        for (char c : text.toCharArray()) {
+            // Add index of '?'if character is out of delimited alphabet
+            if (c < 'A' || c > 'Z')
+                indexes.add(26); //
+            else {
+                for (char letter : alphabet.toCharArray()) {
+                    if (c == letter) {
+                        indexes.add(alphabet.indexOf(letter));
+                    }
+                }
+            }
+        }
+
+        // prints the result line by line, character by character
+        for (int j = 0; j < H; j++) {
+            for (int k = 0; k < indexes.size(); k++) {
+                System.out.print(ascii.get(j).substring(indexes.get(k) * L, indexes.get(k) * L + L));
+            }
+            System.out.println();
+        }
+
+        // close scanner
+        in.close();
+
     }
 }
