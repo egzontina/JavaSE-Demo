ce1fc62ae1a1e9efbfa4b68cc0417e7416035dd8
Malick <diop-malick@hotmail.fr>
2016-09-23 16:51:48 +0200

    Add maven projet for java Cetification

---
+++ JavaCertification/src/main/java/OCA_1Z0_808/App.java
@@ -0,0 +1,13 @@
+package OCA_1Z0_808;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
