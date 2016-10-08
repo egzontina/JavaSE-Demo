ea7bb72fc094e7b1809a2a08d7c8982173ede448
Malick <diop-malick@hotmail.fr>
2016-10-08 18:47:26 +0200

    add chapter Review Question factory

---
+++ JavaCertification/src/main/java/OCA/ReviewQuestions/Chap6.java
@@ -0,0 +1,195 @@
+package OCA.ReviewQuestions;
+
+/**
+ * Test Class for given snippet in exam question
+ */
+
+public class Chap6 {
+
+    public static void main(String[] args) {
+
+        Q1();
+    }
+
+    /**
+     * QUESTION 1
+     */
+    public static void Q1() {
+
+    }
+    
+    /**
+     * QUESTION 2
+     */
+    public static void Q2() {
+
+    }
+    
+    /**
+     * QUESTION 3
+     */
+    public static void Q3() {
+
+    }
+    
+    /**
+     * QUESTION 4
+     */
+    public static void Q4() {
+
+    }
+    
+    /**
+     * QUESTION 5
+     */
+    public static void Q5() {
+
+    }
+    
+    /**
+     * QUESTION 6
+     */
+    public static void Q6() {
+
+    }
+    
+    /**
+     * QUESTION 7
+     */
+    public static void Q7() {
+
+    }
+    
+    /**
+     * QUESTION 8
+     */
+    public static void Q8() {
+
+    }
+    
+    /**
+     * QUESTION 9
+     */
+    public static void Q9() {
+
+    }
+    
+    /**
+     * QUESTION 10
+     */
+    public static void Q10() {
+
+    }
+    
+    /**
+     * QUESTION 11
+     */
+    public static void Q11() {
+
+    }
+    
+    /**
+     * QUESTION 12
+     */
+    public static void Q12() {
+
+    }
+    
+    /**
+     * QUESTION 13
+     */
+    public static void Q13() {
+
+    }
+    
+    /**
+     * QUESTION 14
+     */
+    public static void Q14() {
+
+    }
+    
+    /**
+     * QUESTION 15
+     */
+    public static void Q15() {
+
+    }
+    
+    /**
+     * QUESTION 16
+     */
+    public static void Q16() {
+
+    }
+
+    /**
+     * QUESTION 17
+     */
+    public static void Q17() {
+
+    }
+    /**
+     * QUESTION 18
+     */
+    public static void Q18() {
+
+    }
+    /**
+     * QUESTION 19
+     */
+    public static void Q19() {
+
+    }
+    /**
+     * QUESTION 20
+     */
+    public static void Q20() {
+
+    }
+    /**
+     * QUESTION 21
+     */
+    public static void Q21() {
+
+    }
+    /**
+     * QUESTION 22
+     */
+    public static void Q22() {
+
+    }
+    /**
+     * QUESTION 23
+     */
+    public static void Q23() {
+
+    }
+    
+    /**
+     * QUESTION 24
+     */
+    public static void Q24() {
+
+    }
+    
+    /**
+     * QUESTION 25
+     */
+    public static void Q25() {
+
+    }
+    
+    /**
+     * QUESTION 26
+     */
+    public static void Q26() {
+
+    }
+    /**
+     * QUESTION 27
+     */
+    public static void Q27() {
+
+    }
+}
