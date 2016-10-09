907a41b10310d39cce2850d6b579379d423777cd
Malick <diop-malick@hotmail.fr>
2016-10-09 02:47:14 +0200

    add chap4 code | design Method and static

---
+++ JavaCertification/src/main/java/OCA/Chap4_DesigningMethods.java
@@ -0,0 +1,130 @@
+package OCA;
+
+/**
+ * @see page 166 of study book
+ *
+ */
+public class Chap4_DesigningMethods {
+
+}
+
+//@formatter:off
+
+/**
+ * Access Modifiers
+ * @see Page 167
+ */
+class testClasse1 {
+    public void walk1() {}
+    // default void walk2() {} // DOES NOT COMPILE
+    // void public walk3() {} // DOES NOT COMPILE
+    void walk4() {}
+}
+
+/**
+ * Optional Specifiers
+ * @see Page 168
+ */
+class testClasse2 {
+    public void walk1() {}
+    public final void walk2() {}
+    public static final void walk3() {}
+    public final static void walk4() {}
+    // public modifier void walk5() {} // DOES NOT COMPILE
+    // public void final walk6() {} // DOES NOT COMPILE
+    final public void walk7() {}
+}
+
+/**
+ * Return Type
+ * @see Page 169
+ */
+class testClasse3 {
+    public void walk1() { }
+    public void walk2() { return; }
+    public String walk3() { return ""; }
+    // public String walk4() { } // DOES NOT COMPILE
+    // public walk5() { } // DOES NOT COMPILE
+    // String walk6(int a) { if (a == 4) return ""; } // DOES NOT COMPILE
+}
+
+/**
+ * Method Name
+ * @see Page 170
+ */
+class testClasse4 {
+    public void walk1() { }
+    // public void 2walk() { } // DOES NOT COMPILE
+    // public walk3 void() { } // DOES NOT COMPILE
+    public void Walk_$() { }
+    // public void() { } // DOES NOT COMPILE
+}
+
+/**
+ * Parameter List
+ * @see Page 171
+ */
+class testClasse5 {
+    public void walk1() { }
+    // public void walk2 { } // DOES NOT COMPILE
+    public void walk3(int a) { }
+    // public void walk4(int a; int b) { } // DOES NOT COMPILE
+    public void walk5(int a, int b) { } 
+}
+
+/**
+ * Optional Exception List
+ * 
+ * @see Page 171
+ */
+class testClasse6 {
+    public void zeroExceptions() { }
+    public void oneException() throws IllegalArgumentException { }
+    public void twoExceptions() throws IllegalArgumentException, InterruptedException { }
+}
+
+/**
+ * Method Body
+ * 
+ * @see Page 172
+ */
+class testClasse7 {
+    public void walk1() { }
+    //public void walk2; // DOES NOT COMPILE
+    public void walk3(int a) { int name = 5; }
+}
+
+
+/**
+ * Working with Varargs
+ * @see Page 172
+ */
+class testClasse8 {
+    
+    public void walk1(int... nums) { }
+    public void walk2(int start, int... nums) { }
+    // public void walk3(int... nums, int start) { } // DOES NOT COMPILE
+    // public void walk4(int... start, int... nums) { } // DOES NOT COMPILE
+    
+    public static void walk(int start, int... nums) {
+        
+        System.out.println(nums.length);
+        
+        if (nums.length > 2) 
+        System.out.println(nums[1]);
+    }
+    
+    public static void main(String[] args) {
+      walk(1); // 0
+      walk(1, 2); // 1
+      walk(1, 2, 3); // 2
+      walk(1, new int[] {4, 5}); // 2
+      walk(1, 11, 22, 33); // 22
+      walk(1, null); // throws a NullPointerException
+      
+
+    }
+}
+
+
+//@formatter:on
---
+++ JavaCertification/src/main/java/OCA/Chap4_Static.java
@@ -0,0 +1,97 @@
+package OCA;
+
+import java.util.ArrayList;
+
+public class Chap4_Static {
+
+}
+
+/**
+ * @see page 183 on study Book
+ */
+class Static {
+    private String name = "Static class";
+
+    public static void first() {
+    }
+
+    public static void second() {
+    }
+
+    public void third() {
+        System.out.println(name);
+    }
+
+    public static void main(String args[]) {
+        first();
+        second();
+        // third(); // DOES NOT COMPILE
+    }
+}
+
+/**
+ * @see page 184 on study Book
+ */
+class Gorilla {
+    public static int count;
+
+    public static void addGorilla() {
+        count++;
+    }
+
+    public void babyGorilla() {
+        count++;
+    }
+
+    public void announceBabies() {
+        addGorilla();
+        babyGorilla();
+    }
+
+    public static void announceBabiesToEveryone() {
+        addGorilla();
+        // babyGorilla(); // DOES NOT COMPILE, a static method cannot call an instance method
+    }
+
+    public int total;
+    // public static average = total / count; // DOES NOT COMPILE a static variable cannot call an instance variable
+}
+
+/**
+ * Final
+ * 
+ * @see page 185 on study Book
+ */
+class Initializers {
+
+    private static final int NUM_BUCKETS = 45;
+    private static final ArrayList<String> values = new ArrayList<>();
+
+    public static void main(String[] args) {
+        // NUM_BUCKETS = 5; // DOES NOT COMPILE, connot update a final variable
+
+        values.add("changed"); // allowed
+        // values = new ArrayList<>(); // DOES NOT COMPILE, not allowed
+
+    }
+
+    /* static initializer, run hen the class is used first */
+    
+    private static final int NUM_SECONDS_PER_HOUR;
+    static {
+        int numSecondsPerMinute = 60;
+        int numMinutesPerHour = 60;
+        NUM_SECONDS_PER_HOUR = numSecondsPerMinute * numMinutesPerHour;
+    }
+
+    private static int one;
+    private static final int two;
+    private static final int three = 3;
+    // private static final int four; // DOES NOT COMPILE, final variable never gets initialized
+    static {
+        one = 1;
+        two = 2;
+        // three = 3; // DOES NOT COMPILE, not alloweb to asign it again 
+        // two = 4; // DOES NOT COMPILE, seond attemps
+    }
+}
